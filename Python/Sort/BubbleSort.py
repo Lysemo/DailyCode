c7fef5480a9587b74f82b2db6f5c460714d069f3
corWEI <1065238573@qq.com>
2020-11-26 16:04:04 +0800

    Create BubbleSort.py

---
+++ Python/Sort/BubbleSort.py
@@ -0,0 +1,25 @@
+def BubbleSort(arr, reversed=True):
+    """
+    BubbleSort
+    TimeComplex O(n^2)  SpaceComplex O(1)
+    :param arr: list:arr
+    :param reversed: decrease or increase
+    :return: sorted[arr]
+    """
+    lens = len(arr)
+    if reversed:
+        for i in range(lens - 1):
+            for j in range(lens - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j+1], arr[j]
+    else:
+        for i in range(lens - 1):
+            for j in range(lens - i - 1):
+                if arr[j] < arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j+1], arr[j]
+    return arr
+
+
+# Sample
+lis = [5, 4, 7, 9, 6, 8, 2]
+print(BubbleSort(lis, reversed=False))
