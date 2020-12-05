7a4927ac1e4e8df6e164f5543013cb7503cbec3b
Lysemo <39754276+Lysemo@users.noreply.github.com>
2020-12-05 11:52:26 +0800

    Update JZ9.cpp

--- NEWCODER/JZ9.cpp
+++ NEWCODER/JZ9.cpp
@@ -15,8 +15,27 @@ int jumpFloorII(int number) {
     return dp[number];
 }
 
+////时间占用3ms | 空间占用356k
+int jumpFloorII_(int number) {
+    if(number==0 || number==1) return 1;
+    int pre = 1,res;
+    for(int i=2;i<=number;i++){
+        res = pre << 1;
+        pre = res;
+    }
+    return res;
+}
+
+//时间占用3ms | 空间占用352k
+int jumpFloorII__(int number) {
+    if(number==0 || number==1) return 1;
+    return 1 << (number-1);
+}
+
 int main(){
     cout<<jumpFloorII(3) <<endl;
+    cout<<jumpFloorII_(3) <<endl;
+    cout<<jumpFloorII__(3) <<endl;
     system("pause");
     return 1;
 }
