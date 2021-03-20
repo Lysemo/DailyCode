42323631849ecfdde0d89aec05e9676e3f4d5b20
Lysemo <39754276+Lysemo@users.noreply.github.com>
2021-03-20 16:20:06 +0800

    update JZ35 | Lysemo

--- NEWCODER/JZ35.cpp
+++ NEWCODER/JZ35.cpp
@@ -32,10 +32,50 @@ int InversePairs(vector<int> data) {
     int ans = int(cnt%1000000007);
     return ans;
 }
+void merge__(vector<int> &arr,vector<int> &tmp,int l,int mid,int r,int &ret){
+    int i=l,j=mid+1,k=0;
+    while (i<=mid && j<=r){
+        if(arr[i] > arr[j]){
+            tmp[k++] = arr[j++];
+            ret += (mid-i+1);
+            ret %= 1000000007;
+        }
+        else
+        {
+            tmp[k++] = arr[i++];
+        }
+    }
+    while (i<=mid)
+    {
+        tmp[k++] = arr[i++];
+    }
+    while (j<=r)
+    {
+        tmp[k++] = arr[j++];
+    }
+    for(k=0,i=l;i<=r;++i,++k){
+        arr[i] = tmp[k];
+    }  
+}
+
+void merge_sort__(vector<int> &arr,vector<int> &tmp,int l,int r,int &ret){
+    if(l >= r) return;
+    int mid = l + ((r-l)>>1);
+    merge_sort__(arr,tmp,l,mid,ret);
+    merge_sort__(arr,tmp,mid+1,r,ret);
+    merge__(arr,tmp,l,mid,r,ret);
+}
+
+int InversePairs_(vector<int> data){
+    int ret = 0;
+    vector<int> tmp(data.size());
+    merge_sort__(data,tmp,0,data.size()-1,ret);
+    return ret;
+}
 
 int main(){
-    vector<int> data = {1,2,3,4,5,6,7,0};
-    cout << InversePairs(data) << endl;
+    vector<int> data = {7,6,5,4,3,2,1,0};
+    cout << InversePairs_(data) << endl;
     system("pause");
     return 0;
 }
